f2de76531c30510a791d62f17795a02cb12bf6e4
Jannik-Hm <jannik.heym@gmx.de>
2026-01-26 23:23:41 +0000

    first version of bounding box rasterization for triangles

--- triangle.go
+++ triangle.go
@@ -44,3 +44,38 @@ func scanTriangle(triangle Triangle, canvas draw.Image, color color.Color) {
 		}
 	}
 }
+
+func (triangle Triangle) signed_triangle_area() float64 {
+	return .5 * float64((triangle.b.y-triangle.a.y)*(triangle.b.x+triangle.a.x)+
+		(triangle.c.y-triangle.b.y)*(triangle.c.x+triangle.b.x)+
+		(triangle.a.y-triangle.c.y)*(triangle.a.x+triangle.c.x))
+}
+
+func boundingTriangle(triangle Triangle, canvas draw.Image, color color.Color) {
+	if triangle.a.x == triangle.b.x && triangle.b.x == triangle.c.x {
+		return
+	}
+	if triangle.a.y == triangle.b.y && triangle.b.y == triangle.c.y {
+		return
+	}
+	bbminx := int(math.Min(math.Min(float64(triangle.a.x), float64(triangle.b.x)), float64(triangle.c.x)))
+	bbminy := int(math.Min(math.Min(float64(triangle.a.y), float64(triangle.b.y)), float64(triangle.c.y)))
+	bbmaxx := int(math.Max(math.Max(float64(triangle.a.x), float64(triangle.b.x)), float64(triangle.c.x)))
+	bbmaxy := int(math.Max(math.Max(float64(triangle.a.y), float64(triangle.b.y)), float64(triangle.c.y)))
+	total_area := triangle.signed_triangle_area()
+	if total_area < 1 {
+		return // backface culling + discarding triangles that cover less than a pixel // TODO: test / improve
+	}
+
+	for x := bbminx; x <= bbmaxx; x++ {
+		for y := bbminy; y <= bbmaxy; y++ {
+			alpha := Triangle{Point{x, y}, triangle.b, triangle.c}.signed_triangle_area() / total_area
+			beta := Triangle{Point{x, y}, triangle.c, triangle.a}.signed_triangle_area() / total_area
+			gamma := Triangle{Point{x, y}, triangle.a, triangle.b}.signed_triangle_area() / total_area
+			if alpha < 0 || beta < 0 || gamma < 0 {
+				continue // negative barycentric coordinate => the pixel is outside the triangle
+			}
+			canvas.Set(x, y, color)
+		}
+	}
+}
