b7b277e0218b04b3cd72bc6fe6f03fe2f9f7ee66
Jannik-Hm <jannik.heym@gmx.de>
2026-03-02 00:00:52 +0100

    added fixture label helper

---
+++ fixture_label.go
@@ -0,0 +1,34 @@
+package rasterizer
+
+import (
+	MVRTypes "github.com/Patch2PDF/MVR-Parser/pkg/types"
+)
+
+type fixtureLabel struct {
+	fixture              *MVRTypes.Fixture
+	fixture_bounding_box boundingBox
+}
+
+var fixture_label_padding = padding{5, 10, 5, 10}
+
+func drawFixtureLabels(canvas *Canvas) error {
+	for _, label := range canvas.fixture_labels {
+		bb := label.fixture_bounding_box
+		label_text := label.fixture.FixtureID
+
+		width, height := calcLabelDimensions(canvas, label_text)
+		_ = height
+
+		// TODO: using zbuffer implement label positioning logic so that there are no overlaps
+		x := (bb.right-bb.left-width)/2 + bb.left
+		// y := (bb.bottom-bb.top-height)/2 + bb.top
+		y := bb.bottom + 20
+
+		_, _, err := drawLabelBox(canvas, x, y, label_text, fixture_label_padding)
+		_ = err // TODO: once logic for auto positioning is there, reactivate error forwarding
+		// if err != nil {
+		// 	return err
+		// }
+	}
+	return nil
+}
